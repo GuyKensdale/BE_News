f2a1b17eff652f2d9c23b9829cc72139621e043c
GuyKensdale <126467015+GuyKensdale@users.noreply.github.com>
2023-05-10 12:25:53 +0100

    task 4 completed

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controller");
+const { getArticles } = require("./controllers/article.controllers");
 const endPoints = require("./endpoints.json");
-app.get("/api/topics", getTopics);
+
 app.get("/api", (req, res) => {
   res.json(endPoints);
 });
+app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticles);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Not Found" });
 });
 module.exports = app;
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    res.status(500).send({ msg: "Internal Server Error" });
+  }
+});
+
+//catch block
+//500 status code err for anything not covered
