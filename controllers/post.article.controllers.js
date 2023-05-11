e65bc7c813d52d192f1b573a2c11248317f22fb0
GuyKensdale <126467015+GuyKensdale@users.noreply.github.com>
2023-05-11 15:15:34 +0100

    swopping branches

--- controllers/post.article.controllers.js
+++ controllers/post.article.controllers.js
@@ -1,12 +1,18 @@
 const { postCommentToId } = require("../models/postCommentToIdModel");
 exports.postComments = (req, res, next) => {
-  const objId = req.params;
-  const id = objId.article_id;
-  const com = req;
-  console.log(com);
-  postCommentToId(id, com)
-    .then((comment) => {
-      res.status(200).send({ comment });
-    })
-    .catch(next);
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    const objId = req.params;
+    const id = objId.article_id;
+    const com = JSON.parse(body);
+    console.log(com);
+    postCommentToId(id, com)
+      .then((comment) => {
+        res.status(200).send({ comment });
+      })
+      .catch(next);
+  });
 };
