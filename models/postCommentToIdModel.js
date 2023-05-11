b6566a6cb5b2c2f6c99925ee12552fbcff3dc62c
GuyKensdale <126467015+GuyKensdale@users.noreply.github.com>
2023-05-11 15:53:49 +0100

    changing branch

--- models/postCommentToIdModel.js
+++ models/postCommentToIdModel.js
@@ -1,7 +1,12 @@
 const connection = require("../db/connection");
 exports.postCommentToId = (id, com) => {
   return connection
-    .query(`INSERT INTO comments WHERE VALUES $2`, [com])
+    .query(
+      `INSERT INTO comments (article_id, author, body)
+      VALUES ($1, $2, $3)
+      RETURNING *`,
+      [id, com.author, com.body]
+    )
     .then((result) => {
       const comments = result.rows;
       return comments;
